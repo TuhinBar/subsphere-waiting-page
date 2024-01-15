51f058ed092726b0973927a2de4efc19605ba217
Tuhin <tuhinbr99@gmail.com>
2024-01-15 21:01:20 +0530

    refactor:(Frontned):Modified features card

--- frontend/src/components/Features.jsx
+++ frontend/src/components/Features.jsx
@@ -1,46 +1,48 @@
 import React from "react";
-import { PiPersonArmsSpreadLight } from "react-icons/pi";
-import { HiOutlineChatBubbleBottomCenterText } from "react-icons/hi2";
+
 import { FiVideo } from "react-icons/fi";
 import { AiOutlineDollar } from "react-icons/ai";
 import { RiCustomerServiceLine } from "react-icons/ri";
-import { MdOutlineLocalOffer } from "react-icons/md";
+import {
+  MdPeople,
+  MdDashboardCustomize,
+  MdBusiness,
+  MdCardMembership,
+} from "react-icons/md";
 
 import SingleFeaturecard from "./SingleFeaturecard";
 import PeopleCards from "./PeopleCards";
 
 let features = [
   {
-    name: "Search and find experts",
-    desc: "Over 30 business categories to choose from",
-    icon: <PiPersonArmsSpreadLight className="w-7 h-7 text-blue-400" />,
+    name: "Subscription Management",
+    desc: "Optimize subscriptions and membership with intuitive tools.",
+    icon: <MdPeople className="w-7 h-7 text-red-400" />,
   },
   {
-    name: "Message experts directly",
-    desc: "Ask a question and get advice through mobile chat",
-    icon: (
-      <HiOutlineChatBubbleBottomCenterText className="w-7 h-7 text-red-400" />
-    ),
+    name: "Business Registration",
+    desc: "Effortless onboarding for efficient membership management",
+    icon: <MdBusiness className="w-7 h-7 text-blue-400" />,
   },
   {
-    name: "Quick tips from experts",
-    desc: "See your guide's top tips and advice",
-    icon: <FiVideo className="w-7 h-7 text-yellow-400" />,
+    name: "CRM Integration",
+    desc: "Build lasting connections with members using our CRM",
+    icon: <MdCardMembership className="w-7 h-7 text-yellow-400" />,
   },
   {
-    name: "Pay as you go",
-    desc: "Pay for the time you need, no subscription required",
+    name: "Flexible Billing",
+    desc: "Customize billing categories for financial alignment",
     icon: <AiOutlineDollar className="w-7 h-7 text-purple-400" />,
   },
   {
-    name: "24/7 Customer service",
-    desc: "We're here to help. Contact us anytime",
-    icon: <RiCustomerServiceLine className="w-7 h-7 text-green-400" />,
+    name: "Comprehensive Dashboard",
+    desc: "Real-time overview for informed business decisions.",
+    icon: <MdDashboardCustomize className="w-7 h-7 text-pink-400" />,
   },
   {
-    name: "Regular Offers & Discounts",
-    desc: "Get regular offers and discounts on your bookings",
-    icon: <MdOutlineLocalOffer className="w-7 h-7 text-pink-400" />,
+    name: "24/7 Customer Support",
+    desc: "Dedicated team for round-the-clock assistance.",
+    icon: <RiCustomerServiceLine className="w-7 h-7 text-green-400" />,
   },
 ];
 
@@ -48,15 +50,18 @@ const Features = () => {
   return (
     <div className="flex lg:flex-row flex-col mt-6 lg:-mt-[1rem] 2xl:-mt-20 3xl:-mt-40 w-full lg:w-[80%] p-4 mx-auto bg-[#2a3037] rounded-md mb-12 px-8">
       <div className="">
-        <p className="font-semibold text-3xl text-white">How it works</p>
+        <p className="font-semibold text-3xl text-white">
+          What we are offering
+        </p>
         <p className="text-white text-xs font-medium mt-2 mb-4">
-          We make it easy to build and manage membership program for your
+          We make it easy to build and manage{" "}
+          <span className="text-primary">membership program</span> for your
           business{" "}
         </p>
         <div className="flex flex-col lg:flex-row gap-12 lg:gap-0">
           <div className="flex flex-wrap w-[100%] lg:w-[50%] gap-y-12 gap-x-4 h-full justify-around min-w-0 ">
             {features.map((feature, index) => (
-              <SingleFeaturecard feature={feature} index={index} />
+              <SingleFeaturecard feature={feature} index={index} key={index} />
             ))}
           </div>
           <div className="w-[100%] lg:w-[50%] ">
--- frontend/src/components/Inputs.jsx
+++ frontend/src/components/Inputs.jsx
@@ -7,14 +7,15 @@ const EmailInputs = () => {
         placeholder="your@email.com"
         className="border-2 border-light-secondary transition duration-500 text-light-secondary focus:placeholder-transparent bg-transparent rounded-2xl focus:outline-none p-2 w-[60%] md:pl-4"
       />
-      <div className="relative font-semibold text-white bg-secondary rounded-2xl hover:bg-light-secondary transition-all duration-300 border-2 border-secondary hover:border-white p-2 hover:border-opacity-45 whitespace-nowrap md:px-5 cursor-pointer">
-        
-      <img
-        src="https://i.imgur.com/BL6mFmK.png"
-        alt=""
-        className="absolute md:-top-[330%] md:left-24  md:rotate-[40deg] 3xl:rotate-0 lg:rotate-[30deg] -top-[250%] left-6 lg:left-8 scale-[70%] md:scale-[65%] xl:scale-80 3xl:scale-90 3xl:-left-2"
-      />
-        Join Waitlist
+      <div className="relative">
+        <img
+          src="https://i.imgur.com/BL6mFmK.png"
+          alt=""
+          className="absolute md:-top-[330%] md:left-24  md:rotate-[40deg] 3xl:rotate-0 lg:rotate-[30deg] -top-[250%] left-6 lg:left-8 scale-[70%] md:scale-[65%] xl:scale-80 3xl:scale-90 3xl:-left-2"
+        />
+        <button className="relative font-semibold text-white bg-secondary rounded-2xl hover:bg-light-secondary transition-all duration-300 border-2 border-secondary hover:border-white p-2 hover:border-opacity-45 whitespace-nowrap md:px-5 cursor-pointer">
+          Join Waitlist
+        </button>
       </div>
     </div>
   );
