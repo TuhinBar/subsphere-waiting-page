ba1038c94419467c6a94d987dd4cc635d763b9a8
Souvik Das <103851921+souvik-wizard@users.noreply.github.com>
2024-01-27 20:36:39 +0530

    Footer changes and blogs page added

--- frontend/src/components/BlogPage.jsx
+++ frontend/src/components/BlogPage.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const BlogPage = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-    const { id } = useParams()
+  return (
+    <>
+      <div className="flex justify-between items-center  md:px-8 px-6 py-6">
+        <img src="/LOGO.svg" className="w-8 md:w-12" alt="logo" />
+        <button
+          onClick={() => navigate("/")}
+          className="relative font-semibold text-xs md:text-sm text-white bg-secondary rounded-2xl hover:bg-light-secondary transition-all duration-300 border-2 border-secondary hover:border-white p-2 hover:border-opacity-45 whitespace-nowrap  cursor-pointer"
+        >
+          Join Now
+        </button>
+      </div>
 
-    return (
-        <div>{id}</div>
-    )
-}
+      <div className="text-white">
+        <img
+          src="https://i.imgur.com/IGBjds7.jpg"
+          className="w-[90%] my-8 lg:w-[60%] rounded-lg mx-auto"
+          alt="blogimage"
+        />
 
-export default BlogPage
+        <h1 className="text-xl md:text-2xl font-bold w-[90%] lg:w-2/4 mx-auto text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+          voluptatum.
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default BlogPage;
