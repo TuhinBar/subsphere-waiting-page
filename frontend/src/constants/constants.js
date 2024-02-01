f577bcc13e2707920ea9444230ff1ab1be5de511
Tuhin <85868593+TuhinBar@users.noreply.github.com>
2024-02-01 23:31:33 +0530

    Update constants.js

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = "http://localhost:5000/api/v1/waitlist";
 
-export const BASE_URL = "https://waitlist-backend-kj54.onrender.com/api/va/waitlist";
+export const BASE_URL = "https://waitlist-backend-kj54.onrender.com/api/v1/waitlist";
